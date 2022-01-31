27fab905eb0aa0c522c0341a8da2d2ff4b3252c6
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2022-01-31 11:39:27 -0500

    Update BuffaloRepository.java

--- src/main/java/com/buffalo/Buffalo/BuffaloRepository.java
+++ src/main/java/com/buffalo/Buffalo/BuffaloRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.buffalo.Buffalo.Buffalo;
 import java.util.ArrayList;
 import java.util.List;
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
 
 public interface BuffaloRepository extends CrudRepository<Buffalo, Integer> {
     //List<Buffalo> findBy();
