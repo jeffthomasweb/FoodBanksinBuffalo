d40f0dfb03623040f3865e5083a12c403c4d99ea
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2022-02-02 11:36:29 -0500

    Update MainController.java

--- src/main/java/com/buffalo/Buffalo/MainController.java
+++ src/main/java/com/buffalo/Buffalo/MainController.java
@@ -1,24 +1,50 @@
 package com.buffalo.Buffalo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import java.io.IOException;
+import org.jsoup.*;
+import org.jsoup.helper.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-@Controller
-@CrossOrigin
-//@RequestMapping("/") //(path="/buffalo")  
+@RestController
 public class MainController {
     @Autowired 
     private BuffaloRepository buffaloRepository;
-
-    @CrossOrigin
-    @GetMapping(path="all")
+  
+    @GetMapping(path="/all")
     public @ResponseBody Iterable<Buffalo> getAllBuffalo() {
       return buffaloRepository.findAll();
     }
+
+    @GetMapping("/webscrapenews")
+    public List<String> feed() throws IOException {
+        List<String> results = new ArrayList<String>();
+        Document doc = Jsoup.connect("https://text.npr.org/1001").get();
+        Elements links = doc.getElementsByTag("a");
+	    for (Element link : links) {
+  	    String linkText = link.text();
+  	    results.add(linkText);
+	    }
+
+        List<String> resultsFinal = new ArrayList<String>();
+
+	resultsFinal = results.subList(2,21);
+
+	return resultsFinal;
+}
   }
