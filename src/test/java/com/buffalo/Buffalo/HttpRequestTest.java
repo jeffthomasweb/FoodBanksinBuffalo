0b9abf6f08b91b880d10f40ecb224f2a3864eb3c
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2022-02-02 12:42:07 -0500

    Create HttpRequestTest.java

---
+++ src/test/java/com/buffalo/Buffalo/HttpRequestTest.java
@@ -0,0 +1,34 @@
+package com.buffalo.Buffalo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testApiContainsNameOfFoodPantry() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/all",
+				String.class)).contains("Belle Center Food Pantry");
+	}
+
+	@Test
+        public void testBaseUrlContainsHtml() throws Exception {
+                assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                                String.class)).contains("DOCTYPE");
+        }
+
+}
