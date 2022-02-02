acd15ed6200e3aa92cd0a0e0308ec18f8fea3194
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2022-02-02 18:17:00 -0500

    Update HttpRequestTest.java

--- src/test/java/com/buffalo/Buffalo/HttpRequestTest.java
+++ src/test/java/com/buffalo/Buffalo/HttpRequestTest.java
@@ -18,18 +18,22 @@ public class HttpRequestTest {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
-	
-	//Integration test. Belle Center Food Pantry is the name of a pantry that should be displayed in the API.
+
 	@Test
 	public void testApiContainsNameOfFoodPantry() throws Exception {
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/all",
 				String.class)).contains("Belle Center Food Pantry");
 	}
 
-	//Test that the base URL / displays HTML.
 	@Test
         public void testBaseUrlContainsHtml() throws Exception {
                 assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                                String.class)).contains("DOCTYPE");
+        }
+
+	@Test
+        public void testNewsUrlContainsHtml() throws Exception {
+                assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/news.html",
                                 String.class)).contains("DOCTYPE");
         }
 
