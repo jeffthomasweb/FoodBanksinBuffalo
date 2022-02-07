0a7e0002a5e6a280c9263a4de206aca7f100e02b
Jeff Thomas <93561509+jeffthomasweb@users.noreply.github.com>
2022-02-06 21:21:04 -0500

    Create WebScrape.java

---
+++ src/main/java/com/buffalo/Buffalo/WebScrape.java
@@ -0,0 +1,36 @@
+package com.buffalo.Buffalo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import org.jsoup.*;
+import org.jsoup.helper.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WebScrape {
+	List<String> nprwebscrape() throws IOException {
+            List<String> resultsNpr = new ArrayList<String>();
+            List<String> resultsFinalNpr = new ArrayList<String>();
+	    try {
+                Document docNpr = Jsoup.connect("https://text.npr.org/1001").get();
+                Elements linksNpr = docNpr.getElementsByTag("a");
+                for (Element singleLinkNpr : linksNpr) {
+                    String linkTextNpr = singleLinkNpr.text();
+                    resultsNpr.add(linkTextNpr);
+                }
+                resultsFinalNpr = resultsNpr.subList(2,21);
+            } catch (IOException e) {
+                e.printStackTrace();
+             }
+
+            return resultsFinalNpr;
+    }
+}
